8d01323b6d32222af6ff02bdcbd457436387c568
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-06-15 22:41:04 -0500

    Gallades Calc 0.2.0d - Extra comma oops

--- src/js/data/sets/gen9.js
+++ src/js/data/sets/gen9.js
@@ -1,5 +1,5 @@
 var SETDEX_SV = {
 	"Socknbusk'n": {
 	  "Offensive Pivot": {"level":100,"ability":"Scrappy","item":"Heavy-Duty Boots","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Fake Out","Close Combat","U-turn","Rapid Spin"]}
-	},
+	}
 };
