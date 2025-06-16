73b84236cf1b8afbc5c3644477ca3015c5a3bd6e
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-06-15 23:21:20 -0500

    Gallades Calc 0.2.1 - Added Sets for Set Compendium Page 1

--- src/js/data/sets/gen9.js
+++ src/js/data/sets/gen9.js
@@ -1,9 +1,52 @@
 var SETDEX_SV = {
+	"Arsenstorm": {
+		"Specially Defensive": {"level":100,"ability":"Neutralizing Gas","nature":"Calm","item":"Black Sludge","evs":{"hp":252,"sa":4,"sd":252},"moves":["Earth Power","Mortal Spin","Spikes","Shore Up"]}
+	},
+	"Capricorrie": {
+		"Aurora Veil": {"level":100,"ability":"Snow Warning","item":"Light Clay","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Aurora Veil","High Horsepower","Mountain Gale","Chilly Reception"]},
+		"Choice Band": {"level":100,"ability":"Snow Warning","item":"Choice Band","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Mountain Gale","Ice Shard","High Horsepower","Mighty Cleave"]}
+	},
+	"Fettogre": {
+		"Leech Seed": {"level":100,"ability":"Immunity","item":"Leftovers","nature":"Bold","ivs":{"at":0},"evs":{"hp":248,"df":252,"sd":8},"moves":["Leech Seed","Substitute","Night Shade","Body Press"]},
+		"Defensive Pivot": {"level":100,"ability":"Immunity","item":"Leftovers","nature":"Bold","evs":{"hp":248,"df":252,"sd":8},"moves":["Body Press","Flip Turn","Night Shade","Pain Split"]}
+	},
+	"Frostengu": {
+		"Choice Specs": {"level":100,"ability":"No Guard","item":"Choice Specs","nature":"Timid","evs":{"sa":252,"sd":4,"sp":252},"moves":["Blizzard","Focus Blast","Vacuum Wave","U-turn"]},
+		"Physical Scarf": {"level":100,"ability":"No Guard","item":"Choice Scarf","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["High Jump Kick","Triple Axel","Drill Run","U-turn"]},
+		"Special Scarf": {"level":100,"ability":"No Guard","item":"Choice Scarf","nature":"Timid","evs":{"sa":252,"sd":4,"sp":252},"moves":["Blizzard","Focus Blast","Muddy Water","U-turn"]}
+	},
 	"Hippaint": {
 		"Zpice Forgor": {"level":100,"ability":"Pastel Veil","item":"Loaded Dice","nature":"Timid","ivs":{"at":0},"evs":{"sa":252,"sd":4,"sp":252},"moves":["Work Up","Water Shuriken","Earth Power","Fire Pledge"]}
+	},
+	"Jack-o-swarm": {
+		"Physically Defensive": {"level":100,"ability":"Rough Skin","item":"Rocky Helmet","nature":"Bold","ivs":{"at":0},"evs":{"hp":252,"df":252,"sd":4},"moves":["Stealth Rock","Roost","Air Slash","Earth Power"]},
+		"Specially Defensive": {"level":100,"ability":"Rough Skin","item":"Leftovers","nature":"Bold","ivs":{"at":0},"evs":{"hp":252,"df":252,"sd":4},"moves":["Stealth Rock","Roost","Air Slash","Earth Power"]}
+	},
+	"Mohawtter": {
+		"Physically Defensive": {"level":100,"ability":"Tablets of Ruin","item":"Leftovers","nature":"Calm","ivs":{"at":0},"evs":{"hp":252,"df":240,"sd":16},"moves":["Wish","Burning Bulwark","Scald","Giga Drain"]},
+		"Slow Pivot": {"level":100,"ability":"Tablets of Ruin","item":"Heavy-Duty Boots","nature":"Sassy","ivs":{"at":0},"evs":{"hp":252,"df":4,"sd":252},"moves":["Wish","Burning Bulwark","Scald","Flip Turn"]}
+	},
+	"Pinaturbo": {
+		"Offensive Utility": {"level":100,"ability":"regenerator","item":"Heavy-Duty Boots","nature":"Timid","ivs":{"at":0},"evs":{"hp":248,"sa":8,"sp":252},"moves":["Flamethrower","Parting Shot","Court Change","Will-O-Wisp"]},
+		"Fast Pivot": {"level":100,"ability":"regenerator","item":"Choice Specs","nature":"Timid","ivs":{"at":0},"evs":{"sa":252,"sd":4,"sp":252},"moves":["Flamethrower","Earth Power","Will-O-Wisp","Parting Shot"]},
+		"Choice Specs": {"level":100,"ability":"regenerator","item":"Choice Specs","nature":"Timid","ivs":{"at":0},"evs":{"sa":252,"sd":4,"sp":252},"moves":["Flamethrower","Earth Power","Burn Up","Parting Shot"]}
+	},
+	"Sleet Shell": {
+		"Booster Energy": {"level":100,"ability":"Protosynthesis","item":"Booster Energy","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Swords Dance","Sunsteel Strike","Mighty Cleave","Ice Spinner"]}
 	},
 	"Socknbusk'n": {
 		"Choice Band": {"level":100,"ability":"Scrappy","item":"Choice Band","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Close Combat","U-Turn","Rapid Spin","Drain Punch"]},
 		"Offensive Pivot": {"level":100,"ability":"Scrappy","item":"Heavy-Duty Boots","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Fake Out","Close Combat","U-turn","Rapid Spin"]}
+	},
+	"Thaumaton": {
+		"Choice Scarf": {"level":100,"ability":"Flash Fire","item":"Choice Scarf","nature":"Timid","ivs":{"at":0},"evs":{"sa":252,"sd":4,"sp":252},"moves":["Flash Cannon","Sludge Bomb","Volt Switch","Fire Pledge"]},
+		"Terrain Sweeper": {"level":100,"ability":"Unburden","item":"Grassy Seed","nature":"Timid","ivs":{"at": 0},"evs":{"sa":252,"sd":4,"sp":252},"moves":["Nasty Plot","Sludge Bomb","Flash Cannon","Fire Pledge"]}
+	},
+	"Versalyre": {
+		"Choice Band": {"level":100,"ability":"Protean","item":"Choice Band","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Brave Bird","Mountain Gale","Drill Run","Fire Lash"]},
+		"All-Out Attacker": {"level":100,"ability":"Protean","item":"Heavy-Duty Boots","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Brave Bird","Mountain Gale","Fire Lash","Drill Run"]}
+	},
+	"Vipult": {
+		"Defensive Pivot": {"level":100,"ability":"intimidate","item":"Black Sludge","nature":"Sassy","evs":{"hp":248,"df":8,"sd":252},"moves":["Knock Off","Sludge Bomb","Mortal Spin","U-turn"]}
 	}
 };
