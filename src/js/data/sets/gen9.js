00bc70e1a5879677462c6bdca6f109bd24c4e7f2
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-06-15 22:44:54 -0500

    Gallades Calc 0.2.0e - Testing Multiple Mons

--- src/js/data/sets/gen9.js
+++ src/js/data/sets/gen9.js
@@ -1,5 +1,9 @@
 var SETDEX_SV = {
+	"Hippaint": {
+		"Zpice Forgor": {"level":100,"ability":"Pastel Veil","item":"Loaded Dice","nature":"Timid","ivs":{"at":0},"evs":{"sa":252,"sd":4,"sp":252},"moves":["Work Up","Water Shuriken","Earth Power","Fire Pledge"]}
+	},
 	"Socknbusk'n": {
-	  "Offensive Pivot": {"level":100,"ability":"Scrappy","item":"Heavy-Duty Boots","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Fake Out","Close Combat","U-turn","Rapid Spin"]}
+		"Choice Band": {"level":100,"ability":"Scrappy","item":"Choice Band","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Close Combat","U-Turn","Rapid Spin","Drain Punch"]},
+		"Offensive Pivot": {"level":100,"ability":"Scrappy","item":"Heavy-Duty Boots","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Fake Out","Close Combat","U-turn","Rapid Spin"]}
 	}
 };
