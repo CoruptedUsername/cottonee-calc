ff5860b45c7850d7b8a4b7bc4f172ca629c629eb
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-07-15 16:45:11 -0500

    Gallades Calc 0.5.0f3 - Maybe one-vs-all functionality?

--- src/js/data/sets/gen8.js
+++ src/js/data/sets/gen8.js
@@ -1,6 +1,6 @@
 var SETDEX_SS = {
   "Diglett": {
     "OU Eviolite": {"level":100,"ability":"Arena Trap","item":"Eviolite","nature":"Jolly","evs":{"hp":36,"at":156,"sd":76,"sp":236},"moves":["Earthquake","Beat Up","Final Gambit","Rock Slide"]},
-    "DNUVGC Life Orb": {"level":50,"ability":"Arena Trap","item":"Life Orb","nature":"Jolly","evs":{"hp":36,"at":236,"sp":236},"moves":["Earthquake","Beat Up","Substitute","Rock Slide"]},
+    "DNUVGC Life Orb": {"level":50,"ability":"Arena Trap","item":"Life Orb","nature":"Jolly","evs":{"hp":36,"at":236,"sp":236},"moves":["Earthquake","Beat Up","Substitute","Rock Slide"]}
   }
 };
