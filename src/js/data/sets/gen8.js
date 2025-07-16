abc5963dbf7599ec942336a2b1c66206d108d268
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-07-16 09:53:38 -0500

    Gallades Calc 0.5.0h - DNU sets added

--- src/js/data/sets/gen8.js
+++ src/js/data/sets/gen8.js
@@ -1,6 +1,195 @@
 var SETDEX_SS = {
-  "Diglett": {
-    "OU Eviolite": {"level":100,"ability":"Arena Trap","item":"Eviolite","nature":"Jolly","evs":{"hp":36,"at":156,"sd":76,"sp":236},"moves":["Earthquake","Beat Up","Final Gambit","Rock Slide"]},
-    "DNUVGC Life Orb": {"level":50,"ability":"Arena Trap","item":"Life Orb","nature":"Jolly","evs":{"hp":36,"at":236,"sp":236},"moves":["Earthquake","Beat Up","Substitute","Rock Slide"]}
-  }
-};
+	"Arrokuda": {
+		"OU Choice Band": {"level":100,"ability":"Swift Swim","item":"Choice Band","nature":"Adamant","evs":{"at":252,"sd":4,"sp":252},"moves":["Liquidation","Flip Turn","Crunch","Poison Jab"]}
+	},
+	"Beldum": {
+		"OU Choice Band": {"level":100,"ability":"Clear Body","item":"Choice Band","nature":"Adamant","evs":{"at":252,"sd":4,"sp":252},"moves":["Iron Head","Zen Headbutt","Steel Beam","Iron Defense"]},
+		"OU Bulky Attacker": {"level":100,"ability":"Clear Body","item":"Eviolite","nature":"Brave","evs":{"hp":252,"at":252,"sa":4},"moves":["Iron Head","Zen Headbutt","Steel Beam","Iron Defense"]}
+	},
+	"Bidoof": {
+		"OU Swords Dance": {"level":100,"ability":"Simple","item":"Eviolite","nature":"Adamant","evs":{"hp":120,"at":252,"df":52,"sp":84},"moves":["Return","Crunch","Quick Attack","Swords Dance"]},
+		"OU Demon Bidoof": {"level":100,"ability":"Simple","item":"Eviolite","nature":"Impish","evs":{"hp":252,"at":4,"df":252},"moves":["Rollout","Amnesia","Curse","Rest"]}
+	},
+	"Bramblin": {
+		"OU Bulk-Invested": {"level":100,"ability":"Wind Rider","item":"Eviolite","nature":"Adamant","evs":{"hp":80,"at":252,"sp":176},"moves":["Power Whip","Poltergeist","Rapid Spin","Strength Sap"]},
+		"OU Offensive": {"level":100,"ability":"Wind Rider","item":"Eviolite","nature":"Adamant","evs":{"at":252,"sd":4,"sp":252},"moves":["Power Whip","Poltergeist","Rapid Spin","Shadow Sneak"]}
+	},
+	"Budew": {
+		"OU Itemless": {"level":100,"ability":"Poison Point","item":"","nature":"Modest","evs":{"hp":232,"sa":252,"sp":24},"moves":["Sludge Bomb","Giga Drain","Synthesis","Shadow Ball"]},
+		"OU Choice Specs": {"level":100,"ability":"Poison Point","item":"Choice Specs","nature":"Modest","evs":{"hp":40,"sa":252,"sp":216},"ivs":{"at":0},"moves":["Giga Drain","Sludge Bomb","Shadow Ball","Leaf Storm"]}
+	},
+	"Cherubi": {
+		"OU Sun Sweeper": {"level":100,"ability":"Chlorophyll","item":"Eviolite","nature":"Modest","evs":{"sa":252,"sd":4,"sp":252},"moves":["Giga Drain","Weather Ball","Hidden Power [Rock]","Growth"]}
+	},
+	"Cleffa": {
+		"OU Special Wall": {"level":100,"ability":"Magic Guard","item":"Eviolite","nature":"Calm","evs":{"hp":252,"df":4,"sd":252},"ivs":{"at":0},"moves":["Seismic Toss","Toxic","Wish","Protect"]},
+		"OU Physical Wall": {"level":100,"ability":"Magic Guard","item":"Eviolite","nature":"Bold","evs":{"hp":252,"df":252,"sd":4},"ivs":{"at":0},"moves":["Seismic Toss","Toxic","Wish","Protect"]},
+		"OU Calm Mind": {"level":100,"ability":"Magic Guard","item":"Eviolite","nature":"Bold","evs":{"hp":252,"df":252,"sa":4},"ivs":{"at":0},"moves":["Draining Kiss","Stored Power","Calm Mind","Soft-Boiled"]}
+	},
+	"Cottonee": {
+		"OU Physical Utility": {"level":100,"ability":"Prankster","item":"Eviolite","nature":"Lax","evs":{"hp":252,"at":4,"df":252},"moves":["Giga Drain","Knock Off","Toxic","Defog"]},
+		"OU Cotton Guard": {"level":100,"ability":"Prankster","item":"Eviolite","nature":"Calm","evs":{"hp":252,"sa":4,"sd":252},"ivs":{"at":0},"moves":["Giga Drain","Toxic","Cotton Guard","Leech Seed"]},
+		"OU Switcheroo": {"level":100,"ability":"Prankster","item":"Lagging Tail","nature":"Bold","evs":{"hp":252,"df":252,"sa":4},"ivs":{"at":0},"moves":["Giga Drain","Defog","Toxic","Switcheroo"]}
+	},
+	"Gossifleur": {
+		"OU Physical Utility": {"level":100,"ability":"Regenerator","item":"Heavy-Duty Boots","nature":"Bold","evs":{"hp":252,"df":252,"sd":4},"moves":["Giga Drain","Rapid Spin","Aromatherapy","Stun Spore"]},
+		"OU Special Utility": {"level":100,"ability":"Regenerator","item":"Heavy-Duty Boots","nature":"Calm","evs":{"hp":252,"df":4,"sd":252},"moves":["Giga Drain","Rapid Spin","Aromatherapy","Stun Spore"]}
+	},
+	"Hatenna": {
+		"OU Resttalk": {"level":100,"ability":"Magic Bounce","item":"Eviolite","nature":"Bold","evs":{"hp":248,"df":252,"sa":4,"sp":4},"ivs":{"at":0},"moves":["Psychic","Calm Mind","Rest","Sleep Talk"]},
+		"OU Offensive Support": {"level":100,"ability":"Magic Bounce","item":"Eviolite","nature":"Modest","evs":{"hp":248,"sa":252,"sp":8},"moves":["Psychic","Mystical Fire","Nuzzle","Healing Wish"]}
+	},
+	"Hoothoot": {
+		"OU Double Dance": {"level":100,"ability":"Tinted Lens","item":"Eviolite","nature":"Modest","evs":{"hp":248,"df":8,"sa":252},"ivs":{"at":0},"moves":["Air Slash","Roost","Agility","Nasty Plot"]},
+		"OU Offensive Defog": {"level":100,"ability":"Tinted Lens","item":"Eviolite","nature":"Modest","evs":{"hp":248,"df":8,"sa":252},"ivs":{"at":0},"moves":["Air Slash","Roost","Defog","Nasty Plot"]}
+	},
+	"Jigglypuff": {
+		"OU Mixed Wall": {"level":100,"ability":"Cute Charm","item":"Eviolite","nature":"Calm","evs":{"hp":4,"df":252,"sd":252},"ivs":{"at":0},"moves":["Seismic Toss","Toxic","Wish","Protect"]},
+		"OU Wish Passer": {"level":100,"ability":"Cute Charm","item":"Eviolite","nature":"Bold","evs":{"hp":252,"df":252,"sd":4},"ivs":{"at":0},"moves":["Seismic Toss","Teleport","Wish","Protect"]}
+	},
+	"Kirlia": {
+		"OU Offensive Encore": {"level":100,"ability":"Synchronize","item":"Eviolite","nature":"Timid","evs":{"sa":252,"sd":4,"sp":252},"ivs":{"at":0},"moves":["Psychic","Mystical Fire","Thunderbolt","Encore"]},
+		"OU Choice Scarf": {"level":100,"ability":"Synchronize","item":"Choice Scarf","nature":"Timid","evs":{"sa":252,"sd":4,"sp":252},"ivs":{"at":0},"moves":["Psychic","Mystical Fire","Alluring Voice","Trick"]}
+	},
+	"Litwick": {
+		"OU Itemless": {"level":100,"ability":"Flame Body","item":"temless (Litwick)","nature":"Modest","evs":{"hp":248,"df":104,"sa":156},"ivs":{"at":0},"moves":["Flamethrower","Shadow Ball","Energy Ball","Will-O-Wisp"]},
+		"OU Resttalk": {"level":100,"ability":"Flame Body","item":"Eviolite","nature":"Modest","evs":{"hp":248,"sa":252,"sd":8},"ivs":{"at":0},"moves":["Fire Blast","Shadow Ball","Rest","Sleep Talk"]},
+		"OU Offensive Trick Room": {"level":100,"ability":"Flame Body","item":"Eviolite","nature":"Quiet","evs":{"hp":248,"sa":252,"sd":8},"ivs":{"at":0},"moves":["Fire Blast","Energy Ball","Calm Mind","Trick Room"]},
+		"OU Choice Specs": {"level":100,"ability":"Flame Body","item":"Choice Specs","nature":"Modest","evs":{"hp":248,"sa":252,"sp":8},"moves":["Shadow Ball","Fire Blast","Energy Ball","Overheat"]}
+	},
+	"Luvdisc": {
+		"OU Offensive Pivot": {"level":100,"ability":"Swift Swim","item":"Heavy-Duty Boots","nature":"Timid","evs":{"hp":96,"sa":252,"sp":160},"moves":["Scald","Ice Beam","Flip Turn","Toxic"]},
+		"OU Rain Attacker": {"level":100,"ability":"Swift Swim","item":"Life Orb","nature":"Timid","evs":{"sa":252,"sd":4,"sp":252},"ivs":{"at":0},"moves":["Hydro Pump","Surf","Ice Beam","Rain Dance"]}
+	},
+	"Makuhita": {
+		"OU Bulk Up": {"level":100,"ability":"Guts","item":"Eviolite","nature":"Adamant","evs":{"at":252,"df":116,"sd":128,"sp":12},"moves":["Drain Punch","Knock Off","Heavy Slam","Bulk Up"]}
+	},
+	"Marill": {
+		"OU Physical Wall": {"level":100,"ability":"Sap Sipper","item":"Eviolite","nature":"Impish","evs":{"hp":248,"at":4,"df":252,"sp":4},"moves":["Seismic Toss","Knock Off","Rest","Sleep Talk"]}
+	},
+	"Meditite": {
+		"OU Special Setup": {"level":100,"ability":"Telepathy","item":"Eviolite","nature":"Timid","evs":{"sa":252,"sd":4,"sp":252},"moves":["Psyshock","Vacuum Wave","Calm Mind","Recover"]},
+		"OU Choice Specs": {"level":100,"ability":"Telepathy","item":"Choice Specs","nature":"Timid","evs":{"df":4,"sa":252,"sp":252},"ivs":{"at":0},"moves":["Psyshock","Focus Blast","Vacuum Wave","Shadow Ball"]}
+	},
+	"Nacli": {
+		"OU Mixed Wall": {"level":100,"ability":"Purifying Salt","item":"Eviolite","nature":"Careful","evs":{"hp":248,"df":44,"sd":216},"moves":["Rock Slide","Earthquake","Recover","Stealth Rock"]}
+	},
+	"Nidoran-F": {
+		"OU Special Attacker": {"level":100,"ability":"Poison Point","item":"Eviolite","nature":"Modest","evs":{"hp":248,"df":8,"sa":252},"moves":["Sludge Bomb","Earth Power","Ice Beam","Toxic Spikes"]},
+		"OU Hustle": {"level":100,"ability":"Hustle","item":"Eviolite","nature":"Adamant","evs":{"hp":252,"at":252,"sd":4},"moves":["Poison Jab","Sucker Punch","Hone Claws","Super Fang"]}
+	},
+	"Nymble": {
+		"OU Choice Band": {"level":100,"ability":"Tinted Lens","item":"Choice Band","nature":"Adamant","evs":{"at":252,"df":4,"sp":252},"moves":["First Impression","U-turn","Leech Life","Assurance"]},
+		"OU Boots Pivot": {"level":100,"ability":"Tinted Lens","item":"Heavy-Duty Boots","nature":"Adamant","evs":{"hp":252,"at":252,"sd":4},"moves":["First Impression","U-turn","Leech Life","Assurance"]}
+	},
+	"Pikipek": {
+		"OU Swords Dance": {"level":100,"ability":"Skill Link","item":"Eviolite","nature":"Adamant","evs":{"at":252,"sp":252},"moves":["Brave Bird","Knock Off","Bullet Seed","Swords Dance"]},
+		"OU Choice Band": {"level":100,"ability":"Skill Link","item":"Choice Band","nature":"Adamant","evs":{"at":252,"sd":4,"sp":252},"moves":["Brave Bird","Bullet Seed","Knock Off","U-turn"]}
+	},
+	"Rattata": {
+		"OU Guts": {"level":100,"ability":"Guts","item":"Flame Orb","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Facade","Crunch","U-turn","Iron Tail"]}
+	},
+	"Rattata-Alola": {
+		"OU Choice Scarf": {"level":100,"ability":"Hustle / Thick Fat","item":"Choice Scarf","nature":"Adamant","evs":{"at":252,"sd":4,"sp":252},"moves":["Pursuit","U-turn","Double-Edge","Crunch"]},
+		"OU Pursuit Pivot": {"level":100,"ability":"Thick Fat","item":"Heavy-Duty Boots","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Pursuit","Crunch","Return","U-turn"]}
+	},
+	"Rockruff @ Choice Ban": {
+		"OU Rockruff @ Choice Ba": {"level":100,"ability":"Own Tempo","item":"Choice Band","nature":"Adamant","evs":{"at":252,"sd":4,"sp":252},"moves":["Stone Edge","Stomping Tantrum","Iron Head","Crunch"]}
+	},
+	"Rockruff @ Eviolit": {
+		"OU Rockruff @ Evioli": {"level":100,"ability":"Own Tempo","item":"Eviolite","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Stone Edge","Stomping Tantrum","Sucker Punch","Swords Dance"]}
+	},
+	"Rockruff": {
+		"OU Hazard Lead": {"level":100,"ability":"Own Tempo","item":"Focus Sash","nature":"Hasty","evs":{"at":252,"sp":252},"ivs":{"hp":0,"df":0,"sd":0},"moves":["Rock Slide","Taunt","Endeavor","Stealth Rock"]}
+	},
+	"Roggenrola": {
+		"OU Bulky Attacker": {"level":100,"ability":"Sturdy","item":"Eviolite","nature":"Adamant","evs":{"hp":252,"at":252,"sd":4},"moves":["Rock Slide","Earthquake","Explosion","Stealth Rock"]},
+		"OU Special Tank": {"level":100,"ability":"Sturdy","item":"Eviolite","nature":"Careful","evs":{"hp":168,"sd":252,"sp":88},"moves":["Rock Slide","Body Press","Iron Defense","Rest"]}
+	},
+	"Surskit": {
+		"OU Special Attacker": {"level":100,"ability":"Swift Swim","item":"Eviolite","nature":"Modest","evs":{"sa":252,"sd":4,"sp":252},"moves":["Surf","Ice Beam","Bug Buzz","Toxic"]},
+		"OU Choice Specs": {"level":100,"ability":"Swift Swim","item":"Choice Specs","nature":"Modest","evs":{"sa":252,"sd":4,"sp":252},"ivs":{"at":0},"moves":["Hydro Pump","Ice Beam","Bug Buzz","Shadow Ball"]}
+	},
+	"Swablu": {
+		"OU Physical Wall": {"level":100,"ability":"Natural Cure","item":"Eviolite","nature":"Impish","evs":{"hp":252,"df":252,"sd":4},"moves":["Brave Bird","Toxic","Defog","Roost"]}
+	},
+	"Swinub": {
+		"OU Wallbreaker": {"level":100,"ability":"Oblivious","item":"Life Orb","nature":"Adamant","evs":{"at":252,"sd":4,"sp":252},"moves":["Earthquake","Icicle Crash","Ice Shard","Stealth Rock"]}
+	},
+	"Tadbulb": {
+		"OU Choice Scarf": {"level":100,"ability":"Static","item":"Choice Scarf","nature":"Naive","evs":{"at":252,"sa":4,"sp":252},"moves":["Volt Switch","Thunderbolt","Muddy Water","Hyper Voice"]},
+		"OU Bulky Attacker": {"level":100,"ability":"Static","item":"Eviolite","nature":"Modest","evs":{"hp":252,"df":4,"sa":252},"ivs":{"at":0},"moves":["Discharge","Muddy Water","Volt Switch","Acid Spray"]}
+	},
+	"Taillow": {
+		"OU Guts": {"level":100,"ability":"Guts","item":"Flame Orb","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Facade","U-turn","Pursuit","Protect"]},
+		"OU Choice Specs": {"level":100,"ability":"Scrappy","item":"Choice Specs","nature":"Timid","evs":{"sa":252,"sd":4,"sp":252},"moves":["Boomburst","Air Slash","Heat Wave","U-turn"]}
+	},
+	"Venipede": {
+		"OU Hazard Lead": {"level":100,"ability":"Speed Boost","item":"Focus Sash / Eviolite","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Poison Jab","Endeavor","Spikes","Toxic Spikes"]}
+	},
+	"Wooper-Paldea": {
+		"OU Special Wall": {"level":100,"ability":"Water Absorb / Unaware","item":"Eviolite","nature":"Careful","evs":{"hp":252,"at":4,"sd":252},"moves":["Earthquake","Poison Jab","Stealth Rock","Recover"]},
+		"OU Physical Wall": {"level":100,"ability":"Water Absorb / Unaware","item":"Eviolite","nature":"Impish","evs":{"hp":252,"at":4,"df":252},"moves":["Earthquake","Poison Jab","Stealth Rock","Recover"]}
+	},
+	"Yungoos": {
+		"OU Choice Band": {"level":100,"ability":"Stakeout / Adaptability","item":"Choice Band","nature":"Jolly","evs":{"at":252,"df":4,"sp":252},"moves":["Return","Crunch","Earthquake","U-turn"]},
+		"OU Choice Scarf": {"level":100,"ability":"Stakeout / Adaptability","item":"Choice Scarf","nature":"Jolly","evs":{"at":252,"df":4,"sp":252},"moves":["Return","Crunch","Earthquake","U-turn"]}
+	},
+	"Zigzagoon-Galar": {
+		"OU Quick Feet Pivot": {"level":100,"ability":"Quick Feet","item":"Toxic Orb","nature":"Adamant","evs":{"at":252,"sd":8,"sp":252},"moves":["Facade","Knock Off","Parting Shot","Protect"]}
+	},
+	"Diglett": {
+		"OU Hazard Lead": {"level":100,"ability":"Sand Force","item":"Focus Sash","nature":"Jolly","evs":{"hp":248,"at":8,"sp":252},"moves":["Earthquake","Endeavor","Stealth Rock","Memento"]},
+		"OU Swords Dance": {"level":100,"ability":"Sand Force","item":"Eviolite","nature":"Jolly","evs":{"hp":80,"at":252,"sp":176},"moves":["Earthquake","Rock Slide","Sucker Punch","Swords Dance"]}
+	},
+	"Happiny": {
+		"OU Special Wall": {"level":100,"ability":"Natural Cure","item":"Eviolite","nature":"Calm","evs":{"hp":252,"df":4,"sd":252},"ivs":{"at":0},"moves":["Seismic Toss","Toxic","Rest","Sleep Talk"]},
+		"OU Mixed Wall": {"level":100,"ability":"Natural Cure","item":"Eviolite","nature":"Bold","evs":{"hp":252,"df":252,"sd":4},"ivs":{"at":0},"moves":["Seismic Toss","Counter","Rest","Sleep Talk"]}
+	},
+	"Impidimp": {
+		"OU Nasty Plot": {"level":100,"ability":"Prankster","item":"Eviolite","nature":"Modest","evs":{"hp":252,"sa":252,"sd":4},"ivs":{"at":0},"moves":["Dark Pulse","Draining Kiss","Parting Shot","Nasty Plot"]},
+		"OU Screens Setter": {"level":100,"ability":"Prankster","item":"Light Clay","nature":"Calm","evs":{"hp":252,"df":4,"sd":252},"ivs":{"at":0},"moves":["Reflect","Light Screen","Parting Shot","Taunt"]}
+	},
+	"Mareep": {
+		"OU Choice Specs": {"level":100,"ability":"Static","item":"Choice Specs","nature":"Modest","evs":{"hp":252,"sa":252,"sd":4},"ivs":{"at":0},"moves":["Volt Switch","Thunderbolt","Signal Beam","Hidden Power [Ice]"]}
+	},
+	"Noibat @ Choice Spec": {
+		"OU Noibat @ Choice Spe": {"level":100,"ability":"Infiltrator","item":"Choice Specs","nature":"Modest","evs":{"hp":248,"sa":252,"sd":8},"moves":["Draco Meteor","Hurricane","U-turn","Switcheroo"]}
+	},
+	"Pidove": {
+		"OU Physical Defog": {"level":100,"ability":"Super Luck","item":"Heavy-Duty Boots","nature":"Impish","evs":{"hp":248,"df":252,"sd":8},"moves":["Dual Wingbeat","Defog","Roost","U-turn"]}
+	},
+	"Rolycoly": {
+		"OU Utility Support": {"level":100,"ability":"Flash Fire","item":"Eviolite","nature":"Calm","evs":{"hp":248,"df":28,"sd":232},"moves":["Power Gem","Rapid Spin","Will-O-Wisp","Stealth Rock"]}
+	},
+	"Shinx": {
+		"OU Rain Support": {"level":100,"ability":"Intimidate","item":"Eviolite","nature":"Calm","evs":{"hp":248,"df":200,"sa":4,"sd":56},"ivs":{"at":0},"moves":["Thunder","Volt Switch","Hidden Power [Ice]","Toxic"]}
+	},
+	"Slugma": {
+		"OU Physical Wall": {"level":100,"ability":"Flame Body","item":"Eviolite","nature":"Bold","evs":{"hp":248,"df":252,"sa":8},"ivs":{"at":0},"moves":["Lava Plume","Earth Power","Recover","Stealth Rock"]}
+	},
+	"Snorunt": {
+		"OU Choice Band": {"level":100,"ability":"Inner Focus","item":"Choice Band","nature":"Adamant","evs":{"hp":248,"at":252,"sp":8},"moves":["Icicle Crash","Ice Shard","Switcheroo","Spikes"]}
+	},
+	"Spinarak": {
+		"OU Hone Claws": {"level":100,"ability":"Sniper","item":"Eviolite","nature":"Adamant","evs":{"hp":248,"at":252,"sd":8},"moves":["Megahorn","Poison Jab","Knock Off","Hone Claws"]}
+	},
+	"Starly": {
+		"OU Final Gambit": {"level":100,"ability":"Reckless","item":"Choice Band","nature":"Adamant","evs":{"at":252,"sd":4,"sp":252},"moves":["Double-Edge","Brave Bird","U-turn","Final Gambit"]}
+	},
+	"Tarountula": {
+		"OU Hazard Setter": {"level":100,"ability":"Insomnia","item":"Eviolite","nature":"Impish","evs":{"hp":252,"df":252,"sd":4},"moves":["Knock Off","Circle Throw","Sticky Web","Toxic Spikes"]}
+	},
+	"Wimpod": {
+		"OU Rain Setter": {"level":100,"ability":"Wimp Out","item":"Damp Rock","nature":"Jolly","evs":{"hp":252,"at":4,"sp":252},"moves":["Spikes","Taunt","Rain Dance","Scald"]}
+	},
+	"Wooloo": {
+		"OU Physical Wall": {"level":100,"ability":"Fluffy","item":"Eviolite","nature":"Impish","evs":{"hp":248,"df":252,"sd":8},"moves":["Facade","Copycat","Rest","Sleep Talk"]}
+	},
+	"Wooper": {
+		"OU Physical Wall": {"level":100,"ability":"Unaware","item":"Eviolite","nature":"Impish","evs":{"hp":252,"at":4,"df":252},"moves":["Earthquake","Recover","Spikes","Scald"]}
+	},
+	"Zubat": {
+		"OU Special Defog": {"level":100,"ability":"Infiltrator","item":"Eviolite","nature":"Gentle","evs":{"hp":248,"at":8,"sd":252},"moves":["Sludge Bomb","Brave Bird","Defog","Roost"]}
+	}
+}
