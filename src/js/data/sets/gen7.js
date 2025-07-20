a26d83c0ff22f365b3d2bf85873a44607d162416
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-07-20 15:19:11 -0500

    Gallades Calc 0.6.0b - Fixing dummy set

--- src/js/data/sets/gen7.js
+++ src/js/data/sets/gen7.js
@@ -1,5 +1,5 @@
 var SETDEX_SM = {
   "Aerodactyl": {
-    "Literally required for the calc not to crash": {"ability":"Infiltrator","nature":"Serious"},
+    "Literally required for the calc not to crash": {"ability":"Infiltrator","nature":"Serious", "moves": ['Ancient Power']},
   }
 };
