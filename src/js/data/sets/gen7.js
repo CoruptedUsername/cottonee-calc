e4ef4287da89c5731c8fdbc91d932ab05cc63d1f
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-07-20 15:23:23 -0500

    Gallades Calc 0.6.0d - Fixing dummy set

--- src/js/data/sets/gen7.js
+++ src/js/data/sets/gen7.js
@@ -1,5 +1,5 @@
 var SETDEX_SM = {
   "Aerodactyl": {
-	  "Literally Required": {"ability":"Infiltrator","item":"Heavy-Duty Boots","nature":"Jolly","evs":{"at":252,"df":4,"sp":252},"moves":["Ancient Power","Aqua Tail","Crunch","Agility"]},
+	  "Literally Required": {"ability":"Infiltrator","item":"Heavy-Duty Boots","nature":"Jolly","evs":{"at":252,"df":4,"sp":252},"moves":["Ancient Power","Aqua Tail","Crunch","Agility"]}
   }
 };
