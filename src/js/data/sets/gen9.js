40f7a1168ad43cdbcd5a5cbf5d87d0efa87be7b2
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-08-27 20:25:52 -0500

    Cottonees Calc 1.3.1c - Blank Canvas Meta C Slate 1

--- src/js/data/sets/gen9.js
+++ src/js/data/sets/gen9.js
@@ -174,32 +174,32 @@ var SETDEX_SV = {
 		"MetaA Specially Defensive": {"level":100,"ability":"Mold Breaker","item":"Heavy-Duty Boots","nature":"Careful","evs":{"hp":248,"df":8,"sd":252},"moves":["Fire Lash","Mortal Spin","Toxic Spikes","Slack Off"]}
 	},
 	"Goodjur": {
-		"MetaC Calm Mind": {"level":100,"ability":"Natural Cure","item":"Leftovers","nature":"timid","evs":{"sa":252,"sd":4,"sp":252},"moves":["Psychic","Secret Sword","Thunderbolt","Calm Mind"]},
-		"MetaC Scarf Pivot": {"level":100,"ability":"Natural Cure","item":"Choice Scarf","nature":"modest","evs":{"hp":252,"sa":252,"sd":4},"moves":["Psychic","Secret Sword","Volt Switch","Trick"]}
+		"MetaC Calm Mind": {"level":100,"ability":"Natural Cure","item":"Leftovers","nature":"Timid","evs":{"sa":252,"sd":4,"sp":252},"moves":["Psychic","Secret Sword","Thunderbolt","Calm Mind"]},
+		"MetaC Scarf Pivot": {"level":100,"ability":"Natural Cure","item":"Choice Scarf","nature":"Modest","evs":{"hp":252,"sa":252,"sd":4},"moves":["Psychic","Secret Sword","Volt Switch","Trick"]}
 	},
 	"Liftaton": {
-		"MetaC Bulky Attacker": {"level":100,"ability":"Mold Breaker","item":"Leftovers","nature":"adamant","evs":{"hp":252,"at":252,"sd":4},"moves":["Iron Head","Mighty Cleave","Bullet Punch","Lunar Blessing"]},
-		"MetaC Slow Disruptor": {"level":100,"ability":"Mold Breaker","item":"Heavy-Duty Boots","nature":"modest","evs":{"hp":252,"sa":252,"sd":4},"moves":["Flash Cannon","Toxic","Volt Switch","Roar"]}
+		"MetaC Bulky Attacker": {"level":100,"ability":"Mold Breaker","item":"Leftovers","nature":"Adamant","evs":{"hp":252,"at":252,"sd":4},"moves":["Iron Head","Mighty Cleave","Bullet Punch","Lunar Blessing"]},
+		"MetaC Slow Disruptor": {"level":100,"ability":"Mold Breaker","item":"Heavy-Duty Boots","nature":"Modest","evs":{"hp":252,"sa":252,"sd":4},"moves":["Flash Cannon","Toxic","Volt Switch","Roar"]}
 	},
 	"Surgarou": {
-		"MetaC Fast Attacker": {"level":100,"ability":"Regenerator","item":"Heavy-Duty Boots","nature":"jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Thunder Punch","Drill Peck","Taunt","Volt Switch"]},
-		"MetaC Hazard Lead": {"level":100,"ability":"Regenerator","item":"Focus Sash","nature":"jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Stone Axe","Rapid Spin","Thunder Wave","Volt Switch"]}
+		"MetaC Fast Attacker": {"level":100,"ability":"Regenerator","item":"Heavy-Duty Boots","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Thunder Punch","Drill Peck","Taunt","Volt Switch"]},
+		"MetaC Hazard Lead": {"level":100,"ability":"Regenerator","item":"Focus Sash","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["Stone Axe","Rapid Spin","Thunder Wave","Volt Switch"]}
 	},
 	"Treatmint": {
-		"MetaC Slow Pivot": {"level":100,"ability":"Natural Cure","item":"Leftovers","nature":"modest","evs":{"hp":252,"sa":252,"sd":4},"moves":["Matcha Gotcha", "Sludge Bomb", "Wish", "Protect"]},
-		"MetaC Stall Support": {"level":100,"ability":"Natural Cure","item":"Leftovers","nature":"modest","evs":{"hp":252,"sa":252,"sd":4},"moves":["Wish", "Heal Bell", "Toxic", "Matcha Gotcha"]}
+		"MetaC Slow Pivot": {"level":100,"ability":"Natural Cure","item":"Leftovers","nature":"Modest","evs":{"hp":252,"sa":252,"sd":4},"moves":["Matcha Gotcha", "Sludge Bomb", "Wish", "Protect"]},
+		"MetaC Stall Support": {"level":100,"ability":"Natural Cure","item":"Leftovers","nature":"Modest","evs":{"hp":252,"sa":252,"sd":4},"moves":["Wish", "Heal Bell", "Toxic", "Matcha Gotcha"]}
 	},
 	"Triceracotta": {
-		"MetaC Choice Band": {"level":100,"ability":"Technician","item":"Choice Band","nature":"jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["High Horsepower", "Shadow Punch", "Triple Axel", "U-Turn"]},
-		"MetaC Speed Control": {"level":100,"ability":"Technician","item":"Leftovers","nature":"jolly","evs":{"hp":44,"at":252,"sp":212},"moves":["Bulldoze", "Shadow Punch", "Shadow Sneak", "U-Turn"]}
+		"MetaC Choice Band": {"level":100,"ability":"Technician","item":"Choice Band","nature":"Jolly","evs":{"at":252,"sd":4,"sp":252},"moves":["High Horsepower", "Shadow Punch", "Triple Axel", "U-Turn"]},
+		"MetaC Speed Control": {"level":100,"ability":"Technician","item":"Leftovers","nature":"Jolly","evs":{"hp":44,"at":252,"sp":212},"moves":["Bulldoze", "Shadow Punch", "Shadow Sneak", "U-Turn"]}
 	},
 	"Turvarpega": {
-		"MetaC Mid-Speed Attacker": {"level":100,"ability":"Rock Head","item":"Heavy-Duty Boots","nature":"adamant","evs":{"hp":64,"at":252,"sp":192},"moves":["Brave Bird", "Flare Blitz", "Knock Off", "High Horsepower"]},
-		"MetaC Spike Support": {"level":100,"ability":"Rock Head","item":"Heavy-Duty Boots","nature":"adamant","evs":{"hp":252,"at":252,"sd":4},"moves":["Brave Bird", "High Horsepower", "Spikes", "Roost"]}
+		"MetaC Mid-Speed Attacker": {"level":100,"ability":"Rock Head","item":"Heavy-Duty Boots","nature":"Adamant","evs":{"hp":64,"at":252,"sp":192},"moves":["Brave Bird", "Flare Blitz", "Knock Off", "High Horsepower"]},
+		"MetaC Spike Support": {"level":100,"ability":"Rock Head","item":"Heavy-Duty Boots","nature":"Adamant","evs":{"hp":252,"at":252,"sd":4},"moves":["Brave Bird", "High Horsepower", "Spikes", "Roost"]}
 	},
 	"Woolverine": {
-		"MetaC Choice Band": {"level":100,"ability":"Fluffy","item":"Choice Band","nature":"adamant","evs":{"at":252,"df":4,"sd":252},"moves":["Crunch", "Play Rough", "Drain Punch", "Parting Shot"]},
-		"MetaC Bulky Support": {"level":100,"ability":"Fluffy","item":"Leftovers","nature":"adamant","evs":{"hp":252,"at":252,"sd":4},"moves":["Stealth Rock", "Parting Shot", "Spirit Break", "Taunt"]}
+		"MetaC Choice Band": {"level":100,"ability":"Fluffy","item":"Choice Band","nature":"Adamant","evs":{"at":252,"df":4,"sd":252},"moves":["Crunch", "Play Rough", "Drain Punch", "Parting Shot"]},
+		"MetaC Bulky Support": {"level":100,"ability":"Fluffy","item":"Leftovers","nature":"Adamant","evs":{"hp":252,"at":252,"sd":4},"moves":["Stealth Rock", "Parting Shot", "Spirit Break", "Taunt"]}
 
 	}
 };
