0f17525a679fe683eb6eb058de412106055a805e
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-12-07 10:42:06 -0600

    Cottonees Calc 1.5.0d - Initial addition of MHOU - Forgot initial set

--- src/js/data/sets/gen4.js
+++ src/js/data/sets/gen4.js
@@ -1,5 +1,5 @@
 var SETDEX_DPP = {
   "Agnaktor": {
     "Loadbearing Set": {"ability":"Magma Armor","item":"Leftovers","nature":"Bold","evs":{"hp":248,"df":252,"sd":8},"moves":["Acrobatics","Agility","Armor Cannon","Beak Blast"]}
-  },
+  }
 };
