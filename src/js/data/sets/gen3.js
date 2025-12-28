6d2f566367ed9366431d0607fdb3636975b046eb
CoruptedUsername <52797912+CoruptedUsername@users.noreply.github.com>
2025-12-27 20:21:57 -0600

    Cottonees Calc 1.6.0c - Touhoumons Initial Upload - Oops, forgot to set the new dex

--- src/js/data/sets/gen3.js
+++ src/js/data/sets/gen3.js
@@ -1,5 +1,5 @@
 var SETDEX_ADV = {
   "Alice Margatroid": {
     "Loadbearing Set": {"ability":"Analytic","item":"Leftovers","nature":"Quiet","evs":{"hp":248,"at":8,"sa":252},"moves":["Aura Sphere","Aurora Veil","Blizzard","Brick Break"]}
-  },
+  }
 };
